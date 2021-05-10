cf54047a5f856d062e4926288b95729f36c932e5
Prakash5590 <40067886+Prakash5590@users.noreply.github.com>
2021-05-10 13:05:07 +0530

    Add files via upload

---
+++ src/main/java/com/salt/pepper/CurrencyValueService/controller/CurrencyValueController.java
@@ -0,0 +1,37 @@
+package com.salt.pepper.CurrencyValueService.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.salt.pepper.CurrencyValueService.model.Currency;
+import com.salt.pepper.CurrencyValueService.service.CurrencyValueService;
+
+@RestController
+public class CurrencyValueController {
+	
+	@Autowired
+	private CurrencyValueService service;
+	
+	@GetMapping("/retrieve/from/{from}/to/{to}")
+	public ResponseEntity<Currency> convert(@PathVariable String from , @PathVariable String to) {
+		System.out.println("In controller from : "+from +" to :"+to);
+		Currency curr = new Currency();
+		curr.setFromCurrencyType(from);
+		curr.setToCurrencyType(to);
+		Currency updated = service.getCurrencyObject(curr);
+		System.out.println("returnign--->"+updated);
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(updated);
+	}
+	
+	@PostMapping("/save")
+	public ResponseEntity<Currency> saveCurrencyValue(@RequestBody Currency currency){
+		service.saveCurrency(currency);
+		return ResponseEntity.status(HttpStatus.CREATED).body(currency);
+	}
+}
---
+++ src/test/java/com/salt/pepper/CurrencyValueService/CurrencyValueServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salt.pepper.CurrencyValueService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyValueServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
