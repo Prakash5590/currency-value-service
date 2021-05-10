b00e09411a2499521dedc8d6c039609ce6fb4f14
Prakash5590 <40067886+Prakash5590@users.noreply.github.com>
2021-05-10 12:45:26 +0530

    Add files via upload

---
+++ CurrencyValueServiceApplication.java
@@ -0,0 +1,13 @@
+package com.salt.pepper.CurrencyValueService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyValueServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyValueServiceApplication.class, args);
+	}
+
+}
